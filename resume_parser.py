6ba532b09000cb1eea91296be17578dcd34109de
danilalobnya <sobolevdaniil67@gmail.com>
2024-07-01 18:07:50 +0300

    added resume db

--- resume_parser.py
+++ resume_parser.py
@@ -1,9 +1,37 @@
 import time
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+import random
+
+
+def create_table():
+
+    db = sqlite3.connect('resume.db')
+
+    c = db.cursor()
+
+    c.execute("""CREATE TABLE IF NOT EXISTS resume (
+        id integer PRIMARY KEY,
+        name text,
+        age integer,
+        salary text,
+        tags text,
+        link text
+        )
+    """)
+
+    db.commit()
+
+    db.close()
+
+    print("таблица создана")
 
 
 def get_links(text):
+
+    create_table()
+
     data = requests.get(
         url=f"https://hh.ru/search/resume?text={text}&area=1&isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&page=1",
         headers={"User-Agent": "User-Agent"}
@@ -31,6 +59,7 @@ def get_links(text):
 
 
 def get_data(link):
+
     data = requests.get(
         url=link,
         headers={"User-Agent": "User-Agent"}
@@ -38,6 +67,7 @@ def get_data(link):
     if data.status_code != 200:
         return
     soup = BeautifulSoup(data.content, "lxml")
+    id = random.randint(1, 10000)
     try:
         name = soup.find(attrs={"class":"resume-block__title-text"}).text
     except:
@@ -54,6 +84,22 @@ def get_data(link):
         tags = [tag.text for tag in soup.find(attrs={"class":"bloko-tag-list"}).find_all(attrs={"class":"bloko-tag__section_text"})]
     except:
         tags = []
+
+    # запись в базу данных
+
+    db = sqlite3.connect('resume.db')
+
+    c = db.cursor()
+
+    c.execute('''INSERT OR IGNORE INTO resume
+    (id, name, age, salary, tags, link) 
+    VALUES (?, ?, ?, ?, ?, ?)
+    ''', (id, name, age, salary, tags[0], link))
+
+    db.commit()
+
+    db.close()
+
     resume = {
         "name": name,
         "age": age,
